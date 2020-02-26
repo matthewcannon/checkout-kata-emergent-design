be54aa3468ab3945f9c62757888de72a0be5a5f4
matthew.cannon@cdl.co.uk <matthew.cannon@cdl.co.uk>
2020-02-26 14:20:28 +0000

    Pass the second test.

--- src/main/java/uk/co/supermarket/Checkout.java
+++ src/main/java/uk/co/supermarket/Checkout.java
@@ -6,6 +6,10 @@ public class Checkout {
 
     private final int itemPriceA;
 
+    private final char itemCodeB;
+
+    private final int itemPriceB;
+
     public Checkout(char itemCodeA, int itemPriceA) {
         this(itemCodeA, itemPriceA, ' ', 0);
     }
@@ -13,11 +17,17 @@ public class Checkout {
     public Checkout(char itemCodeA, int itemPriceA, char itemCodeB, int itemPriceB) {
         this.itemCodeA = itemCodeA;
         this.itemPriceA = itemPriceA;
+        this.itemCodeB = itemCodeB;
+        this.itemPriceB = itemPriceB;
     }
 
     public int scan(char itemCode) {
         if (itemCode == itemCodeA) {
             return itemPriceA;
+        }
+
+        if (itemCode == itemCodeB) {
+            return itemPriceB;
         }
 
         return 0;
