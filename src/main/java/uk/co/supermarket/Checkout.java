7d32c5a36cc49e86a37888901217b38ba7171aea
matthew.cannon@cdl.co.uk <matthew.cannon@cdl.co.uk>
2020-02-26 13:56:42 +0000

    Pass the first test.

--- src/main/java/uk/co/supermarket/Checkout.java
+++ src/main/java/uk/co/supermarket/Checkout.java
@@ -3,6 +3,10 @@ package uk.co.supermarket;
 public class Checkout {
 
     public int scan(char itemCode) {
+        if (itemCode == 'A') {
+            return 50;
+        }
+
         return 0;
     }
 }
