d234a43a195e3900f3b379332334d27061b965c6
matthew.cannon@cdl.co.uk <matthew.cannon@cdl.co.uk>
2020-03-03 12:44:53 +0000

    Pass the fifth test by introducing a mutable primitive map.

--- src/main/java/uk/co/supermarket/PricingCatalogue.java
+++ src/main/java/uk/co/supermarket/PricingCatalogue.java
@@ -1,27 +1,26 @@
 package uk.co.supermarket;
 
+import org.eclipse.collections.api.map.primitive.MutableCharIntMap;
+import org.eclipse.collections.impl.map.mutable.primitive.CharIntHashMap;
+
 public class PricingCatalogue {
 
-    private char itemCode;
-
-    private int itemPrice;
+    private MutableCharIntMap items;
 
     public PricingCatalogue(char itemCode, int itemPrice) {
-        this.itemCode = itemCode;
-        this.itemPrice = itemPrice;
+        this();
+        addItem(itemCode, itemPrice);
     }
 
     public PricingCatalogue() {
+        items = new CharIntHashMap();
     }
 
     public int priceOf(char itemCode) {
-        if (itemCode == this.itemCode) {
-            return itemPrice;
-        }
-
-        return 0;
+        return items.get(itemCode);
     }
 
     public void addItem(char itemCode, int itemPrice) {
+        items.addToValue(itemCode, itemPrice);
     }
 }
